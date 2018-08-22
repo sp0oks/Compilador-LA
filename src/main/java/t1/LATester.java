8b1f3f695b5f7fb9a0af3e666212acee6444742f
spooks <g4briel.4lves@gmail.com>
2018-08-22 20:27:38 -0300

    Update main class

--- src/main/java/t1/LATester.java
+++ src/main/java/t1/LATester.java
@@ -6,6 +6,9 @@ import org.antlr.v4.runtime.RecognitionException;
 import org.antlr.v4.runtime.misc.ParseCancellationException;
 
 import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.File;
 import java.io.IOException;
 import java.io.FileNotFoundException;
 
@@ -15,19 +18,37 @@ public class LATester {
         SaidaParser sp = new SaidaParser();
         try {
             ANTLRInputStream input = new ANTLRInputStream(new FileInputStream(args[0]));
-            LALexer lexer = new LALexer(input);
+	    LALexer lexer = new LALexer(input);
             CommonTokenStream tokens = new CommonTokenStream(lexer);
             LAParser parser = new LAParser(tokens);
-            parser.addErrorListener(new ErrorListener(sp));
+ 
+	    parser.addErrorListener(new ErrorListener(sp));
             parser.programa();
-        } catch (ParseCancellationException pce) {
+	} catch (ParseCancellationException pce) {
             if (pce.getMessage() != null) {
                 sp.println(pce.getMessage());
             }
         } catch (ArrayIndexOutOfBoundsException iobe)  {
-	    System.out.println("Erro: nenhum argumento foi dado ao executar o compilador.");
+	    System.out.println("Erro: nenhum arquivo de entrada foi dado ao executar o compilador.");
 	} catch (FileNotFoundException fnfe) {
 	    System.out.println("Erro: o arquivo passado no argumento não foi encontrado."); 
+	}
+	
+	String out = sp.toString();
+	if (!out.equals("")) {
+	    try {
+		File fd = new File(args[1]);
+		fd.createNewFile();
+	        PrintWriter pw = new PrintWriter(new FileWriter(fd));
+		pw.println(out);
+		pw.close();
+	    } catch (ArrayIndexOutOfBoundsException iobe) {
+		File fd = new File("saida.txt");
+		fd.createNewFile();
+	        PrintWriter pw = new PrintWriter(new FileWriter(fd));
+		pw.println(out);
+		pw.close();
+	    }
 	}
     }
 }
