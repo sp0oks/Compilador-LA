e146835e4a53cb9c28c853af757438fb379a6be3
spooks <g4briel.4lves@gmail.com>
2018-08-23 15:43:58 -0300

    Atualizando mensagens de erro

---
+++ src/main/java/t1/LABaseVisitor.java
@@ -0,0 +1,365 @@
+// Generated from /home/spooks/github/UFSCar/Compilador-LA/src/main/java/t1/LA.g4 by ANTLR 4.7
+package t1;
+
+import org.antlr.v4.runtime.tree.AbstractParseTreeVisitor;
+
+/**
+ * This class provides an empty implementation of {@link LAVisitor},
+ * which can be extended to create a visitor which only needs to handle a subset
+ * of the available methods.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+public class LABaseVisitor<T> extends AbstractParseTreeVisitor<T> implements LAVisitor<T> {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitPrograma(LAParser.ProgramaContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitDeclaracoes(LAParser.DeclaracoesContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitDecl_local_global(LAParser.Decl_local_globalContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitDeclaracao_local(LAParser.Declaracao_localContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitVariavel(LAParser.VariavelContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitIdentificador(LAParser.IdentificadorContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitDimensao(LAParser.DimensaoContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitTipo(LAParser.TipoContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitTipo_basico(LAParser.Tipo_basicoContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitTipo_basico_ident(LAParser.Tipo_basico_identContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitTipo_estendido(LAParser.Tipo_estendidoContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitValor_constante(LAParser.Valor_constanteContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitRegistro(LAParser.RegistroContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitDeclaracao_global(LAParser.Declaracao_globalContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitParametro(LAParser.ParametroContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitParametros(LAParser.ParametrosContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitCorpo(LAParser.CorpoContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitCmd(LAParser.CmdContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitCmdLeia(LAParser.CmdLeiaContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitCmdEscreva(LAParser.CmdEscrevaContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitCmdSe(LAParser.CmdSeContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitCmdCaso(LAParser.CmdCasoContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitCmdPara(LAParser.CmdParaContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitCmdEnquanto(LAParser.CmdEnquantoContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitCmdFaca(LAParser.CmdFacaContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitCmdAtribuicao(LAParser.CmdAtribuicaoContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitCmdChamada(LAParser.CmdChamadaContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitCmdRetorne(LAParser.CmdRetorneContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitSelecao(LAParser.SelecaoContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitItem_selecao(LAParser.Item_selecaoContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitConstantes(LAParser.ConstantesContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitNumero_intervalo(LAParser.Numero_intervaloContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitOp_unario(LAParser.Op_unarioContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitExp_aritmetica(LAParser.Exp_aritmeticaContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitTermo(LAParser.TermoContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitFator(LAParser.FatorContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitOp1(LAParser.Op1Context ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitOp2(LAParser.Op2Context ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitOp3(LAParser.Op3Context ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitParcela(LAParser.ParcelaContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitParcela_unario(LAParser.Parcela_unarioContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitParcela_nao_unario(LAParser.Parcela_nao_unarioContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitExp_relacional(LAParser.Exp_relacionalContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitOp_relacional(LAParser.Op_relacionalContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitExpressao(LAParser.ExpressaoContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitTermo_logico(LAParser.Termo_logicoContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitFator_logico(LAParser.Fator_logicoContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitParcela_logica(LAParser.Parcela_logicaContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitOp_logico_1(LAParser.Op_logico_1Context ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitOp_logico_2(LAParser.Op_logico_2Context ctx) { return visitChildren(ctx); }
+}
---
+++ src/main/java/t1/LAVisitor.java
@@ -0,0 +1,314 @@
+// Generated from /home/spooks/github/UFSCar/Compilador-LA/src/main/java/t1/LA.g4 by ANTLR 4.7
+package t1;
+
+import org.antlr.v4.runtime.tree.ParseTreeVisitor;
+
+/**
+ * This interface defines a complete generic visitor for a parse tree produced
+ * by {@link LAParser}.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+public interface LAVisitor<T> extends ParseTreeVisitor<T> {
+	/**
+	 * Visit a parse tree produced by {@link LAParser#programa}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitPrograma(LAParser.ProgramaContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#declaracoes}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitDeclaracoes(LAParser.DeclaracoesContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#decl_local_global}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitDecl_local_global(LAParser.Decl_local_globalContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#declaracao_local}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitDeclaracao_local(LAParser.Declaracao_localContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#variavel}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitVariavel(LAParser.VariavelContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#identificador}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitIdentificador(LAParser.IdentificadorContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#dimensao}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitDimensao(LAParser.DimensaoContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#tipo}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitTipo(LAParser.TipoContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#tipo_basico}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitTipo_basico(LAParser.Tipo_basicoContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#tipo_basico_ident}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitTipo_basico_ident(LAParser.Tipo_basico_identContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#tipo_estendido}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitTipo_estendido(LAParser.Tipo_estendidoContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#valor_constante}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitValor_constante(LAParser.Valor_constanteContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#registro}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitRegistro(LAParser.RegistroContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#declaracao_global}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitDeclaracao_global(LAParser.Declaracao_globalContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#parametro}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitParametro(LAParser.ParametroContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#parametros}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitParametros(LAParser.ParametrosContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#corpo}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitCorpo(LAParser.CorpoContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#cmd}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitCmd(LAParser.CmdContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#cmdLeia}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitCmdLeia(LAParser.CmdLeiaContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#cmdEscreva}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitCmdEscreva(LAParser.CmdEscrevaContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#cmdSe}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitCmdSe(LAParser.CmdSeContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#cmdCaso}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitCmdCaso(LAParser.CmdCasoContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#cmdPara}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitCmdPara(LAParser.CmdParaContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#cmdEnquanto}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitCmdEnquanto(LAParser.CmdEnquantoContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#cmdFaca}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitCmdFaca(LAParser.CmdFacaContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#cmdAtribuicao}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitCmdAtribuicao(LAParser.CmdAtribuicaoContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#cmdChamada}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitCmdChamada(LAParser.CmdChamadaContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#cmdRetorne}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitCmdRetorne(LAParser.CmdRetorneContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#selecao}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitSelecao(LAParser.SelecaoContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#item_selecao}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitItem_selecao(LAParser.Item_selecaoContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#constantes}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitConstantes(LAParser.ConstantesContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#numero_intervalo}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitNumero_intervalo(LAParser.Numero_intervaloContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#op_unario}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitOp_unario(LAParser.Op_unarioContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#exp_aritmetica}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitExp_aritmetica(LAParser.Exp_aritmeticaContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#termo}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitTermo(LAParser.TermoContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#fator}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitFator(LAParser.FatorContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#op1}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitOp1(LAParser.Op1Context ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#op2}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitOp2(LAParser.Op2Context ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#op3}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitOp3(LAParser.Op3Context ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#parcela}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitParcela(LAParser.ParcelaContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#parcela_unario}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitParcela_unario(LAParser.Parcela_unarioContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#parcela_nao_unario}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitParcela_nao_unario(LAParser.Parcela_nao_unarioContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#exp_relacional}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitExp_relacional(LAParser.Exp_relacionalContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#op_relacional}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitOp_relacional(LAParser.Op_relacionalContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#expressao}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitExpressao(LAParser.ExpressaoContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#termo_logico}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitTermo_logico(LAParser.Termo_logicoContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#fator_logico}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitFator_logico(LAParser.Fator_logicoContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#parcela_logica}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitParcela_logica(LAParser.Parcela_logicaContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#op_logico_1}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitOp_logico_1(LAParser.Op_logico_1Context ctx);
+	/**
+	 * Visit a parse tree produced by {@link LAParser#op_logico_2}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitOp_logico_2(LAParser.Op_logico_2Context ctx);
+}
